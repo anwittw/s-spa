6b0380b9fbb00fe4e1d809c7de3461223ce70cf7
anwittw <git@andrewittwer.de>
2019-06-19 11:01:49 +0100

    Added ccs-variable for Background, wired up toogleBtn

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+let $navElements = document.querySelectorAll("#nav-elements>a");
+let $toogleButton1 = document.getElementById("toogle1");
+
+let $root = document.documentElement;
+
+
+
+
+
 function listenForClicks() {
-  let $navElements = document.querySelectorAll("#nav-elements>a");
+
   // console.log($navElements)
+
+  $toogleButton1.addEventListener("click", function(e) {
+    // toogleActiveClass($toogleButton1);
+    toogleActiveClass($toogleButton1,'active')
+    setBackground()
+  });
+
   $navElements.forEach($navElement =>
     $navElement.addEventListener("click", function(e) {
       e.preventDefault();
@@ -28,3 +44,27 @@ function initialRender() {
 }
 
 initialRender();
+
+function toogleActiveClass(el,className) 
+{
+  el.classList.toggle(className)
+}
+
+function changeBackground() {
+
+}
+
+
+function setBackground() {
+if($toogleButton1.classList.contains('active')) {
+  console.log('night')
+  $root.style.setProperty('--bg-img','url(images/night.jpg');
+} else {
+  console.log('day')
+  $root.style.setProperty('--bg-img','url(images/day.jpg');
+}
+}
+
+
+
+
